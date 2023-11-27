6d6e0551fc84e8b18e4ffe9c7b48471522c396b0
uwnik <uwnik@LAPTOP-15UE0H1F>
2023-11-27 04:26:48 +0400

    changed parse_reviews, add write_reviews

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ soup = BeautifulSoup(r.text, "lxml")
 path = r"C:\Users\uwnik\OneDrive\Рабочий стол"
 projectname = "dataset"
 folders = \
-["0",
- "1",
+["1",
  "2",
  "3",
  "4",
@@ -24,11 +23,23 @@ for f in folders:
     if not os.path.exists(folder):
         os.mkdir(folder)
 
-def parse_reviews():
+def parse_reviews(rating):
     reviews = soup.findAll("div", class_= f"lenta-card")
     review_texts = []
     for review in reviews:
-        name_book = review.find("a", class_=f"lenta-card__book-title").text.strip()
-        review_text = review.find("div", id_="lenta-card__text-review-full").text.strip()
-        review_texts.append(review_text)
+        rate = review.find("span", class_=f"lenta-card__mymark").text.strip()
+        if rate == rating:
+            name_book = review.find("a", class_=f"lenta-card__book-title").text.strip()
+            review_text = review.find("div", id_="lenta-card__text-review-full").text.strip()
+            review_texts.append(name_book + "\n" + review_text)
     return review_texts
+
+def write_reviews(reviews, rating, count):
+    for review_text in reviews:
+        if count < 1000:
+            with open(fr"C:\Users\uwnik\OneDrive\Рабочий стол\dataset\{rating}\{rating}_{count:04}.txt", "w", encoding="utf-8") as file:
+                file.write(review_text)
+            count += 1
+        else:
+            break
+    return count
